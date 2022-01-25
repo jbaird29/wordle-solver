de80b668923e4a7c18beca5d178dcc80bf416c94
Jon Baird <jbaird29@gmail.com>
2022-01-24 19:22:48 -0500

    solution 3 - completes solution solver

--- run_trial.py
+++ run_trial.py
@@ -1,33 +1,32 @@
 from wordle_solver import WordleSolver
 from statistics import mean
-from functools import reduce
 
-def run_trial(answer_word: str) -> int:
+
+def run_trial(w: WordleSolver, answer: str) -> int:
     """
     Runs the optimizer and returns the number of guesses until it got the answer
-    :param answer_word: the word that is the puzzle's correct solution
-    :return:
+    :param answer: the word that is the puzzle's correct solution
     """
     guess_count = 0
-    w = WordleSolver("wordle-allowed.txt", "wordle-answers.txt", method="average")
-    bv = w._answer_word_to_bitvector(answer_word)
     while True:
         guess_count += 1
         guess = w.get_optimal_guess()
-        if guess == answer_word:
+        if guess == answer:
+            w.reset_game()
             return guess_count
-        feedback = w._feedback_bitvector_to_str(bv & w._guess_word_to_mask(guess))
+        feedback = w._calculate_feedback(answer, guess)
         w.update_with_guess_feedback(feedback)
         if w.get_potential_answers_length() == 0:
             raise Exception('Answers list is zero')
 
 
 if __name__ == '__main__':
-    words = WordleSolver._load_words("wordle-answers.txt")
+    w = WordleSolver("dictionaries/wordle-allowed.txt", "dictionaries/wordle-answers.txt", method="worst")
+    words = WordleSolver._load_words("dictionaries/wordle-answers.txt")
     guess_counts = []
     above_6 = []
     for i, word in enumerate(words):
-        count = run_trial(word)
+        count = run_trial(w, word)
         guess_counts.append(count)
         if count > 6:
             above_6.append(word)
--- wordle_solver.py
+++ wordle_solver.py
@@ -1,48 +1,72 @@
+import collections
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, Counter
 from functools import reduce
-
+import os
+import json
 
 class WordleSolver:
-    # TODO - need to fix a 'false yellow' bug -> e.g. knoll & stoop returns BBGYB rather than BBGBB
 
-    def __init__(self, guess_file, answer_file="", method=""):
+    def __init__(self, guess_file: str, answer_file="", method=""):
+        g_file = os.path.splitext(os.path.split(guess_file)[-1])[0]
+        a_file = os.path.splitext(os.path.split(answer_file)[-1])[0]
+        self._save_file = os.path.join('', 'feedbacks', f"{a_file}__{g_file}.json")
+        self._method = method  # either "average" or "worst"
         self._guess_words = self._load_words(guess_file)
         self._answer_words = self._load_words(answer_file) if answer_file != "" else [w for w in self._guess_words]
-        self._answer_bitvectors = [0] * len(self._answer_words)
-        self._method = method
-        self._guesses_made = []  # the guesses that were made
-        self._feedbacks_given = []  # the feedbacks given on those guesses
-        # associate each possible answer word with its bitvector
-        for j in range(len(self._answer_words)):
-            self._answer_bitvectors[j] = self._answer_word_to_bitvector(self._answer_words[j])
+        self._answers_remaining = enumerate(self._answer_words)  # mutable -> filtered as guesses are made
+        self._guesses_made = []  # mutable -> the guesses that were made -> tuple of (index: int, word: str)
+        self._feedbacks_given = []  # mutable -> the feedbacks given on those guesses -> str
+        try:
+            with open(self._save_file, 'r') as infile:
+                print("Loading the feedbacks JSON file...")
+                self._feedbacks = json.load(infile)
+        except FileNotFoundError:
+            print("Creating the feedbacks JSON file...")
+            os.makedirs(os.path.dirname(self._save_file), exist_ok=True)
+            self._feedbacks = self._populate_feedbacks()
+            with open(self._save_file, 'w') as outfile:
+                json.dump(self._feedbacks, outfile)
+        except json.decoder.JSONDecodeError:
+            print("There was an error processing the feedbacks JSON file. Recreating it...")
+            self._feedbacks = self._populate_feedbacks()
+            with open(self._save_file, 'w') as outfile:
+                json.dump(self._feedbacks, outfile)
+
+    def reset_game(self):
+        self._answers_remaining = enumerate(self._answer_words)
+        self._guesses_made = []
+        self._feedbacks_given = []
 
     def get_optimal_guess(self) -> str:
+        """
+        Returns the optimal guess word, based on the game state and feedback given so far
+        """
         # base case - if there is only one possible answer left, that is the correct solution
-        if len(self._answer_words) == 1:
-            guess = self._answer_words[0]
+        if type(self._answers_remaining) == list and len(self._answers_remaining) == 1:
+            j, guess = self._answers_remaining[0]
+            i = self._get_word_index(guess, "guesses")
         # # optimization - if this is the first guess, go with the pre-calculated best guess
-        elif len(self._guesses_made) == 0 and self._method == "worst":
-            guess = "aesir"
         elif len(self._guesses_made) == 0 and self._method == "average":
-            guess = "roate"
+            i, guess = 7642, "roate"
+        elif len(self._guesses_made) == 0 and self._method == "worst":
+            i, guess = 113, "aesir"
         # otherwise, calculate the best guess based on current game state
         else:
-            guess = self._calculate_optimal_guess()
-        self._guesses_made.append(guess)
+            i, guess = self._calculate_optimal_guess()
+        self._guesses_made.append((i, guess))
         return guess
 
-    def _calculate_optimal_guess(self) -> str:
+    def _calculate_optimal_guess(self) -> (int, str):
         # "factors" holds optimization factors (either worst case or avg); enumerations is the word partitions
         factors = [0] * len(self._guess_words)
         all_enumerations = [defaultdict(int) for _ in range(len(self._guess_words))]
         # for each guessable word - enumerate the possible outcomes & map every answer word to an outcome
-        for i in range(len(self._guess_words)):
+        for i, guess in enumerate(self._guess_words):
             enumerations = all_enumerations[i]
-            mask = self._guess_word_to_mask(self._guess_words[i])
-            for j in range(len(self._answer_words)):
-                feedback_bitvector = self._answer_bitvectors[j] & mask
-                enumerations[feedback_bitvector] += 1
+            for j, answer in self._answers_remaining:
+                feedback = self._feedbacks[i][j]
+                enumerations[feedback] += 1
             # worst case - the outcome with most words left
             if self._method == "worst":
                 factors[i] = max(enumerations.values())
@@ -52,103 +76,75 @@ class WordleSolver:
                 weighted_avgs = [(x / total) * x for x in enumerations.values()]
                 factors[i] = sum(weighted_avgs)
         # return the optimal word - the minimum of the factors
+        # print(sorted([(factors[i], self._guess_words[i]) for i in range(len(factors))])[0:10])  # the top 10 options
         min_index = reduce(lambda p, c: c if factors[c] < factors[p] else p, range(len(factors)))
-        return self._guess_words[min_index]
+        return min_index, self._guess_words[min_index]
 
-    def update_with_guess_feedback(self, feedback_str: str, guess_word="") -> None:
+    def update_with_guess_feedback(self, feedback: str, guess="") -> None:
         """
         Updates the game state with feedback given a guess
-        :param feedback_str: a five-letter string of G (green) Y (yellow) B (black/grey); e.g. if guess was CAB and
+        :param feedback: a five-letter string of G (green) Y (yellow) B (black/grey); e.g. if guess was CAB and
             the solution was BAD, feedback would be the string: "BGY"
-        :param guess_word: the guess that was made; if empty, assumes the guess was the previously given optimal guess
+        :param guess: the guess that was made; if empty, assumes the guess was the previously given optimal guess
         """
-        if guess_word != "":
-            self._guesses_made[-1] = guess_word  # TODO - ensure this method is only called after a get_optimal_guess call
-        guess_word = self._guesses_made[-1]  # e.g. 'raise'
-        self._feedbacks_given.append(feedback_str)
-        # convert the guess and feedback str into a bitvector / mask
-        feedback_bitvector = self._feedback_str_to_bitvector(feedback_str, guess_word)
-        guess_mask = self._guess_word_to_mask(guess_word)
+        self._feedbacks_given.append(feedback)
+        if guess != "":  # TODO - ensure this method is only called after a get_optimal_guess call
+            i = self._get_word_index(guess, "guesses")
+            self._guesses_made[-1] = (i, guess)
+        else:
+            i, guess = self._guesses_made[-1]  # e.g. 'raise'
         # filter the word list (and its associated bitvectors) based on the feedback
-        new_answer_words = []
-        new_answer_bitvectors = []
-        for j in range(len(self._answer_words)):
-            if self._answer_bitvectors[j] & guess_mask == feedback_bitvector:
-                new_answer_words.append(self._answer_words[j])
-                new_answer_bitvectors.append(self._answer_bitvectors[j])
-        self._answer_words = new_answer_words
-        self._answer_bitvectors = new_answer_bitvectors
+        new_answers_remaining = []
+        for j, answer in self._answers_remaining:
+            if self._feedbacks[i][j] == feedback:
+                new_answers_remaining.append((j, self._answer_words[j]))
+        self._answers_remaining = new_answers_remaining
         return
 
     def get_potential_answers_length(self):
+        """
+        Returns the length of possible answers remaining, based on guesses made / feedback given so far
+        """
         return len(self._answer_words)
 
-    @staticmethod
-    def _feedback_str_to_bitvector(feedback: str, guess: str) -> int:
-        """
-        Helper functions; converts the string representation of feedback to a bitvector
-        :param feedback: if no feedback string given, takes the last entry
-        :return: bitvector representation of that feedback
-        """
-        bitvector = ['000'] * 26 * 5  # 001 is green, 010 is yellow, 100 is black (grey)
-        for pos, feedback_letter in enumerate(feedback):  # feedback is "GYBBY"  -> G is pos 0; Y is pos 1; etc.
-            guess_letter = guess[pos]  # the 'r' in 'raise'
-            c = ord(guess_letter) - 97  # ord('a') = 97
-            i = len(bitvector) - 1 - (c + 26 * pos)  # bitvector is right (pos 0, letter A) to left (pos 4, letter Z)
-            if feedback_letter == 'G':
-                bitvector[i] = '001'
-            elif feedback_letter == 'Y':
-                bitvector[i] = '010'
-            elif feedback_letter == 'B':
-                bitvector[i] = '100'
-            else:
-                raise Exception('Invalid letter in the feedback string.')
-        return int(''.join(bitvector), 2)
+    def _populate_feedbacks(self) -> List[List[str]]:
+        # feedbacks is a matrix of guess_words (rows) x answer_words (cols)
+        feedbacks = [[""] * len(self._answer_words) for _ in range(len(self._guess_words))]
+        for j, answer in enumerate(self._answer_words):
+            answer_letter_counts = Counter(answer)
+            for i, guess in enumerate(self._guess_words):
+                feedback = self._calculate_feedback(answer, guess, answer_letter_counts)
+                feedbacks[i][j] = feedback
+        return feedbacks
+
+    def _get_word_index(self, word, dictionary):
+        if dictionary == 'guesses':
+            return binary_search(self._guess_words, word)
+        elif dictionary == 'answers':
+            return binary_search(self._answer_words, word)
 
     @staticmethod
-    def _feedback_bitvector_to_str(bitvector: int) -> str:
-        feedback = []
-        while bitvector > 0:
-            result = bitvector & 0b111
-            if result == 0b001:
-                feedback.append('G')
-            elif result == 0b010:
-                feedback.append('Y')
-            elif result == 0b100:
-                feedback.append('B')
-            bitvector >>= 3
-        return ''.join(feedback)
-
-    @staticmethod
-    def _answer_word_to_bitvector(word: str) -> int:
-        # 5 positions * 26 letters * 3 outcomes
-        # ordered right to left: position 5..1 ( Z..A [GREY-YELLOW-GREEN] )
-        bitvector = ['000'] * 26 * 5
-        # iterate from back ('a' at pos 0) to front ('z' at pos 4)
-        i = len(bitvector) - 1
-        for p in range(5):
-            for c in range(26):
-                letter = chr(c+97)
-                # set grey if this letter is not in the word at all
-                if letter not in word:
-                    bitvector[i] = '100'
-                # set yellow if this letter is in the word, but not in this position
-                elif word[p] != letter:
-                    bitvector[i] = '010'
-                # set green if this letter matches this position
+    def _calculate_feedback(answer, guess, answer_letter_counts=None):
+        if len(answer) != len(guess):
+            raise Exception('Answer and guess are different lengths')
+        feedback = [''] * len(guess)
+        if answer_letter_counts is None:
+            answer_letter_counts = Counter(answer)  # immutable - passed in as argument for time/space efficiency
+        guess_letter_counts = defaultdict(int)  # mutable - must be created for each guess
+        # first pass - mark the greens
+        for i, letter in enumerate(guess):
+            if letter == answer[i]:
+                feedback[i] = 'G'  # if letters match, this spot is a green
+                guess_letter_counts[letter] += 1
+        # second pass - mark the yellows & blacks (grey)
+        for i, letter in enumerate(guess):
+            if letter != answer[i]:  # skip any spots already marked green
+                if guess_letter_counts[letter] < answer_letter_counts[letter]:
+                    feedback[i] = 'Y'  # if another instance of this letter is left in the word, this spot is yellow
+                    guess_letter_counts[letter] += 1
                 else:
-                    bitvector[i] = '001'
-                # decrement to the next spot in the bitvector
-                i -= 1
-        return int(''.join(bitvector), 2)
-
-    @staticmethod
-    def _guess_word_to_mask(word: str) -> int:
-        mask = 0
-        for pos, letter in enumerate(word):
-            c = ord(letter) - 97  # ord('a') = 97
-            mask |= ((0b111 << 3 * 26 * pos) << 3 * c)
-        return mask
+                    feedback[i] = 'B'  # otherwise this spot is black (grey)
+        return ''.join(feedback)
 
     @staticmethod
     def _load_words(filename: str) -> List[str]:
@@ -156,21 +152,28 @@ class WordleSolver:
             words = f.read().splitlines()
         return words
 
-    @staticmethod
-    def _bitvector_pretty_print(bitvector: int):
-        # TODO - prints a bitvector in a format like:  pos 0 -> a: Y, b: G, c: B, ... \n pos 1 -> a: G, b: B, etc.
-        pass
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
 
 
 if __name__ == '__main__':
-    w = WordleSolver("wordle-allowed.txt", "wordle-answers.txt", method="average")
+    w = WordleSolver("dictionaries/wordle-allowed.txt", "dictionaries/wordle-answers.txt", method="average")
     guess1 = w.get_optimal_guess()
     print(guess1)
     w.update_with_guess_feedback("BYBBB")
-    print(w._answer_words)
     guess2 = w.get_optimal_guess()
     print(guess2)
-    w.update_with_guess_feedback("BGGBG", "snool")
-    print(w._answer_words)
+    w.update_with_guess_feedback("BGGBG")
+    print(w._answers_remaining)
     guess3 = w.get_optimal_guess()
     print(guess3)
